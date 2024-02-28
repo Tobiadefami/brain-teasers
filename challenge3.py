bd41b753c44016439c612bbb2834eef279598684
tobiadefami <tobiadefami@gmail.com>
2024-02-28 13:27:33 +0100

    optimize challenge3

--- challenge3.py
+++ challenge3.py
@@ -1,24 +1,30 @@
 def largest_prime_factor(number: int) -> int:
-    
-    # using the trial division method because my original implementation 
-    # is suboptimal for very large numbers
-    
+
+    # using the trial division method because my original implementation
+    # was suboptimal for very large numbers
+
     # choose the smallest factor to divide the number by
-    smallest_factor:int = 2
-    
+    factor: int = 2
+
     while number > 1:
-        if number % smallest_factor == 0:
-            # update the number based on the
-            number //= smallest_factor
-            while number % smallest_factor == 0:
-                number //= smallest_factor
-        smallest_factor += 1
-        
-    return smallest_factor -1
+        if number % factor == 0:
+            # divide the factor out of the number
+            number //= factor
+            # check if new number is divisible by factor.
+            while number % factor == 0:
+                # if so, divide the factor out of the new number
+                number //= factor
+        # update factor
+        factor += 1
 
+        # check if the square of the factor is greater than the number
+        if factor * factor > number:
+            if number > 1:
+                return number
+            else:
+                break
 
-
-
+    return factor
 
 
 if __name__ == "__main__":
